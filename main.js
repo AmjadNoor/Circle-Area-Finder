c1deb12ef3f492078c6c424b3e2de6e9542ab07a
AmjadNoor <nooramjadmail.com@gmail.com>
2021-02-07 08:58:28 +0530

    Updated project

--- main.js
+++ main.js
@@ -4,18 +4,7 @@ function fn1() {
   let r = document.getElementById('num1').value
   const circleArea = pi*r**2;
   
-/*  switch (fig) {
-    case 'Circle':
-      document.getElementById('para1').innerHTML = 'The area of circle is ' + circleArea;
-      break;
-    default:
-      document.getElementById('para1').innerHTML = 'Please inter valid data.' ;
-  }*/
-  
   if (fig) {
     document.getElementById('para1').innerHTML = 'The area of circle is ' + circleArea;
-  }else
-  {
-    'Please inter valid data.'
   }
 }
