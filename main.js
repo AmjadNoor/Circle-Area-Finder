6d7ba055a88d8a02d847eb24e7fd7af5b930d096
AmjadNoor <nooramjadmail.com@gmail.com>
2021-02-12 08:53:20 +0530

    Updated project

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function fn1() {
   const fig = 'Circle';
   const pi = 3.14159265359;
   let r = document.getElementById('num1').value;
-  const circleArea = pi*r**2;
-  
+  const circleArea = pi * r ** 2;
+
   if (fig) {
     document.getElementById('para1').innerHTML = `The area of circle is ${circleArea}`;
   }
